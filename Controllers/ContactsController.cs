fd7071cc4809d56fb7ce371585d9b3f0192e6d1a
Daniel Kang <danielbkang91@gmail.com>
2020-12-15 02:00:57 -0800

    Implement Post request for contact

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -22,5 +22,26 @@ namespace ContactsApi.Controllers
             return _contactService.GetAll();
         }
 
+        [HttpGet("{id}", Name = "GetContact")]
+        public ActionResult<Contact> GetContact(long id)
+        {
+            var contact = _contactService.Get(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return contact;
+        }
+
+        [HttpPost]
+        public ActionResult<Contact> Create(Contact contact)
+        {
+            _contactService.Create(contact);
+
+            return CreatedAtRoute("GetContact", new { id = contact.Id }, contact);
+        }
+
     }
 }
--- Services/ContactService.cs
+++ Services/ContactService.cs
@@ -1,6 +1,7 @@
 using ContactsApi.Models;
 using Oracle.ManagedDataAccess.Client;
 using System.Data;
+using System;
 using System.Collections.Generic;
 
 namespace ContactsApi.Services
@@ -52,7 +53,71 @@ namespace ContactsApi.Services
             }
             finally
             {
-                _connection.Dispose();
+                _connection.Close();
+            }
+        }
+
+        public Contact Get(long id)
+        {
+            try
+            {
+                if (_connection.State == ConnectionState.Closed)
+                {
+                    _connection.Open();
+                }
+
+                string sql = "select * from " + _tableName
+                    + " where id = " + id;
+
+                OracleCommand command = new OracleCommand(sql, _connection);
+                command.CommandType = CommandType.Text;
+
+                OracleDataReader dataReader = command.ExecuteReader();
+                return dataReader.Read() ? new Contact()
+                {
+                    Id = dataReader.GetInt32(0),
+                    FirstName = dataReader.GetString(1),
+                    LastName = dataReader.GetString(2),
+                    Birth = dataReader.GetDateTime(3),
+                    Email = dataReader.GetString(4),
+                    Address = dataReader.GetString(5),
+                    PhoneNumber = dataReader.GetString(6)
+                } : null;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+        public Contact Create(Contact contact)
+        {
+            try
+            {
+                if (_connection.State == ConnectionState.Closed)
+                {
+                    _connection.Open();
+                }
+
+                string sql = "INSERT INTO " + _tableName
+                    + " values("
+                    + contact.Id + ", "
+                    + "'" + contact.FirstName + "',"
+                    + "'" + contact.LastName + "',"
+                    + "TO_DATE('" + contact.Birth.ToString("yyyy/MM/dd") + "','yyyy/mm/dd'),"
+                    + "'" + contact.Email + "',"
+                    + "'" + contact.Address + "',"
+                    + "'" + contact.PhoneNumber + "')";
+                OracleCommand command = new OracleCommand(sql, _connection);
+                command.CommandType = CommandType.Text;
+
+                command.ExecuteNonQuery();
+
+                return contact;
+            }
+            finally
+            {
+                _connection.Close();
             }
         }
     }
